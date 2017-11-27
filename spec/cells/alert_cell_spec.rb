1e50d94ec752ad4409f752e53392a68783a2e009
Stephen Margheim <margheim@mail.med.upenn.edu>
2017-11-27 16:50:46 -0500

    Finalize specs for AlertCell

--- spec/cells/alert_cell_spec.rb
+++ spec/cells/alert_cell_spec.rb
@@ -13,25 +13,319 @@ RSpec.describe AlertCell do
     context 'when empty instructions' do
       let(:instructions) { {} }
 
-      it { should have_selector('div.alert[role="alert"]') }
-      # it { should_not have_selector('a > span') }
-      # it { should_not have_selector('a > i') }
+      it { should have_selector('div.alert.border-silver[role="alert"]') }
+      it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+      it { should_not have_selector('.alert > .alert-title') }
+      it { should_not have_selector('.alert > .alert-body') }
 
-      # context 'and props are passed' do
-      #   let(:instructions) do
-      #     {
-      #       props: {
-      #         btn: { class: 'test', id: 'btn' },
-      #         text: { class: 'test', id: 'text' },
-      #         icon: { class: 'test', id: 'icon' }
-      #       }
-      #     }
-      #   end
+      context 'and props are passed' do
+        let(:instructions) do
+          {
+            props: {
+              alert: { class: 'test', id: 'alert' },
+              title: { class: 'test', id: 'title' },
+              body: { class: 'test', id: 'body' }
+            }
+          }
+        end
 
-      #   it { should have_selector('a.btn.d-flex.justify-content-between.align-items-center.test#btn[role="button"]') }
-      #   it { should_not have_selector('a > span') }
-      #   it { should_not have_selector('a > i') }
-      # end
+        it { should have_selector('div.alert.border-silver.test#alert[role="alert"]') }
+        it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+        it { should_not have_selector('.alert > .alert-title') }
+        it { should_not have_selector('.alert > .alert-body') }
+      end
+
+      context 'and meta are passed' do
+        context 'where dismissable is true' do
+          let(:instructions) do
+            {
+              meta: {
+                alert: { dismissable: true }
+              }
+            }
+          end
+
+          it { should have_selector('div.alert.alert-dismissable.border-silver[role="alert"]') }
+          it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should_not have_selector('.alert > .alert-title') }
+          it { should_not have_selector('.alert > .alert-body') }
+        end
+
+        context 'where type is specified' do
+          let(:type) { 'warning' }
+          let(:instructions) do
+            {
+              meta: {
+                alert: { type: type }
+              }
+            }
+          end
+
+          it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+          it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should_not have_selector('.alert > .alert-title') }
+          it { should_not have_selector('.alert > .alert-body') }
+        end
+
+        context 'where dismissable is true and type is specified' do
+          let(:type) { 'warning' }
+          let(:instructions) do
+            {
+              meta: {
+                alert: {
+                  dismissable: true,
+                  type: type
+                }
+              }
+            }
+          end
+
+          it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+          it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should_not have_selector('.alert > .alert-title') }
+          it { should_not have_selector('.alert > .alert-body') }
+        end
+      end
+    end
+
+    context 'when `title` only is defined' do
+      context 'as String' do
+        let(:title) { 'TITLE' }
+        let(:instructions) { { title: title } }
+
+        it { should have_selector('div.alert.border-silver[role="alert"]') }
+        it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+        it { should have_selector('.alert > h4.alert-title', text: title) }
+        it { should_not have_selector('.alert > .alert-body') }
+
+        context 'and props are passed' do
+          let(:instructions) { { title: title, props: { title: { class: 'test', id: 'title' } } } }
+
+          it { should have_selector('div.alert.border-silver[role="alert"]') }
+          it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should have_selector('.alert > h4.alert-title.test#title', text: title) }
+          it { should_not have_selector('.alert > .alert-body') }
+        end
+
+        context 'and meta are passed' do
+          context 'where dismissable is true' do
+            let(:instructions) do
+              {
+                title: title,
+                meta: {
+                  alert: { dismissable: true }
+                }
+              }
+            end
+
+            it { should have_selector('div.alert.alert-dismissable.border-silver[role="alert"]') }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should_not have_selector('.alert > .alert-body') }
+          end
+
+          context 'where type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                title: title,
+                meta: {
+                  alert: { type: type }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should_not have_selector('.alert > .alert-body') }
+          end
+
+          context 'where dismissable is true and type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                title: title,
+                meta: {
+                  alert: {
+                    dismissable: true,
+                    type: type
+                  }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should_not have_selector('.alert > .alert-body') }
+          end
+        end
+      end
+    end
+
+    context 'when `body` only is defined' do
+      context 'as String' do
+        let(:body) { 'BODY' }
+        let(:instructions) { { body: body } }
+
+        it { should have_selector('div.alert.border-silver[role="alert"]') }
+        it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+        it { should_not have_selector('.alert > .alert-title') }
+        it { should have_selector('.alert > div.alert-body', text: body) }
+
+        context 'and props are passed' do
+          let(:instructions) { { body: body, props: { body: { class: 'test', id: 'body' } } } }
+
+          it { should have_selector('div.alert.border-silver[role="alert"]') }
+          it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should_not have_selector('.alert > .alert-title') }
+          it { should have_selector('.alert > div.alert-body.test#body', text: body) }
+        end
+
+        context 'and meta are passed' do
+          context 'where dismissable is true' do
+            let(:instructions) do
+              {
+                body: body,
+                meta: {
+                  alert: { dismissable: true }
+                }
+              }
+            end
+
+            it { should have_selector('div.alert.alert-dismissable.border-silver[role="alert"]') }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should_not have_selector('.alert > .alert-title') }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+
+          context 'where type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                body: body,
+                meta: {
+                  alert: { type: type }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should_not have_selector('.alert > .alert-title') }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+
+          context 'where dismissable is true and type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                body: body,
+                meta: {
+                  alert: {
+                    dismissable: true,
+                    type: type
+                  }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should_not have_selector('.alert > .alert-title') }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+        end
+      end
+    end
+
+    context 'when `title` and `body` are both defined' do
+      context 'as Strings' do
+        let(:title) { 'TITLE' }
+        let(:body) { 'BODY' }
+        let(:instructions) { { title: title, body: body } }
+
+        it { should have_selector('div.alert.border-silver[role="alert"]') }
+        it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+        it { should have_selector('.alert > h4.alert-title', text: title) }
+        it { should have_selector('.alert > div.alert-body', text: body) }
+
+        context 'and props are passed' do
+          let(:instructions) do
+            {
+              title: title, body: body,
+              props: {
+                title: { class: 'test', id: 'title' },
+                body: { class: 'test', id: 'body' }
+              }
+            }
+          end
+
+          it { should have_selector('div.alert.border-silver[role="alert"]') }
+          it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+          it { should have_selector('.alert > h4.alert-title.test#title', text: title) }
+          it { should have_selector('.alert > div.alert-body.test#body', text: body) }
+        end
+
+        context 'and meta are passed' do
+          context 'where dismissable is true' do
+            let(:instructions) do
+              {
+                title: title,
+                body: body,
+                meta: {
+                  alert: { dismissable: true }
+                }
+              }
+            end
+
+            it { should have_selector('div.alert.alert-dismissable.border-silver[role="alert"]') }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+
+          context 'where type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                title: title,
+                body: body,
+                meta: {
+                  alert: { type: type }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should_not have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+
+          context 'where dismissable is true and type is specified' do
+            let(:type) { 'warning' }
+            let(:instructions) do
+              {
+                title: title,
+                body: body,
+                meta: {
+                  alert: {
+                    dismissable: true,
+                    type: type
+                  }
+                }
+              }
+            end
+
+            it { should have_selector("div.alert.alert-#{type}[role='alert']") }
+            it { should have_selector('.alert > button.close[data-dismiss="alert"]') }
+            it { should have_selector('.alert > h4.alert-title', text: title) }
+            it { should have_selector('.alert > div.alert-body', text: body) }
+          end
+        end
+      end
     end
   end
 end
